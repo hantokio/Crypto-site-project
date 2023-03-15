cf6275a6407b64c30bbaa5b57b6be065968cedd2
hantokio <maksimenkod96@gmail.com>
2023-03-15 14:59:37 +0300

    our first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function TotalLength(){
+    const path = document.querySelector('#check');
+    const les = Math.round(path.getTotalLength());
+    alert('Длинна ' + les);
+}
+
+
+// console.log('Hello World')
